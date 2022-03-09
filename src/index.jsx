50a9250514942edd8f1f3f0f7249688b61378fce
Kajta01 <37844835+Kajta01@users.noreply.github.com>
2022-03-09 21:16:33 +0100

    working

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
+import Header from './komponenty/Header/Header'
+import Footer from './komponenty/Footer/Footer'
+
+import Dresses from './komponenty/Main/Dresses/Dresses'
+import Shoes from './komponenty/Main/Shoes/Shoes'
+import Accessories from './komponenty/Main/Accessories/Accessories'
+
 const App = () => (
-  <>
-    <p>Tady asi něco bude </p>
-  </>
+<div class="container">
+  <Header title="Moda"/>
+  <Dresses/>
+  <Shoes/>
+  <Accessories/>
+  <Footer year={2022} author="Kamča"/>
+</div>
 );
 
 render(<App />, document.querySelector('#app'));
